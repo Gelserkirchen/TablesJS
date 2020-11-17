a0e2ff0890c6e28a162b7040c76e57f4e3fa249d
gelserkirchen <gelserkirchen@mail.ru>
2020-11-17 07:52:57 +0300

    finished with template table

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -4,20 +4,20 @@ const CODES = {
 }
 
 function toChar(_, index) {
-  String.fromCharCode(CODES.A + index)
-}
-
-function createCell() {
-  return `<div class="cell" contenteditable>B2</div>`
+  return String.fromCharCode(CODES.A + index)
 }
 
 function toColumn(col) {
   return `<div class="column">${col}</div>`
 }
 
-function createRow(content) {
+function toCell() {
+  return `<div class="cell" contenteditable="true"></div>`
+}
+
+function createRow(numbers = '', content) {
   return `<div class="row">
-            <div class="row-info"></div>
+            <div class="row-info">${numbers}</div>
             <div class="row-data">${content}</div>
           </div>`
 }
@@ -25,40 +25,42 @@ function createRow(content) {
 export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
+  const colsWithoutContent = new Array(colsCount)
+      .fill('')
+      .map(toCell)
+      .join('')
 
+  // debugger
   const cols = new Array(colsCount)
       .fill('')
+  // eslint-disable-next-line no-undef
       .map(toChar)
       .map(el => toColumn(el))
       .join('')
 
-  // const rows = new Array(rowsCount)
-  //   .fill('')
-  //   .map(el => to)
-
   console.log(cols);
-  rows.push(createRow(cols));
+  rows.push(createRow('', cols));
 
-  for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow());
+  for (let i = 1; i < rowsCount; i++) {
+    rows.push(createRow(i, colsWithoutContent));
   }
 
   return rows.join('')
 }
 
 //   <div class="row">
-//   <div class="row-info"></div>
-//   <div class="row-data">
-//   <div class="column">A</div>
-//   <div class="column">B</div>
-//   <div class="column">C</div>
-//   </div>
+//      <div class="row-info"></div>
+//      <div class="row-data">
+//          <div class="column">A</div>
+//          <div class="column">B</div>
+//          <div class="column">C</div>
+//      </div>
 //   </div>
 //   <div class="row">
-//   <div class="row-info">1</div>
-//   <div class="row-data">
-//   <div class="cell" contenteditable="true">1</div>
-//   <div class="cell" contenteditable="true">2</div>
-//   <div class="cell" contenteditable="true">3</div>
-//   </div>
+//      <div class="row-info">1</div>
+//      <div class="row-data">
+//           <div class="cell" contenteditable="true">1</div>
+//           <div class="cell" contenteditable="true">2</div>
+//           <div class="cell" contenteditable="true">3</div>
+//      </div>
 //   </div>
